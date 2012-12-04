c57925293b375d4fee344516146cd7b32cd1cd91
Michael Biggs <M.Biggs@2009.Ljmu.ac.uk>
2012-12-04 23:38:00 +0000

    Basic start
    started file with basic of code

---
+++ Assignmnet 1/Assignment 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Assignment1
+{
+    class Program
+    {
+        struct Person
+        {
+            public string FamilyName;
+            public string FirstName;
+            public DateTime birthDate;
+            public string Gender;
+
+        }
+        static void Main(string[] args)
+        {
+            FileStream fs1 = new FileStream(@"M:\test.txt", FileMode.Open);
+            StreamReader sr1 = new StreamReader(fs1);
+            string s;
+            while ((s = sr1.ReadLine()) != null)
+            {
+                Console.WriteLine("line is : " + s);
+
+            }
+            fs1.Close();
+        }
+    }
+}
